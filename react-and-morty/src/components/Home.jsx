47861c854c8c6518cb7a695a92dd91e387e7f491
szegediviktor <szeg.viktor@gmail.com>
2022-05-02 10:47:57 +0200

    add responsive design

--- react-and-morty/src/components/Home.jsx
+++ react-and-morty/src/components/Home.jsx
@@ -1,35 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import background from "../images/background.jpg";
-import { Background, Wrapper, Logo, DescWrapper } from "./Home.styles";
+import {
+    FlexWrapper,
+    Background,
+    ButtonContainer,
+    Logo,
+    Wrapper,
+    Description,
+} from "./Home.styles";
 import logo from "../images/logo.png";
 
 const Home = () => {
-  return (
-    <div>
-      <Background src={background} />
-      <Logo src={logo} />
-      <Wrapper>
-        <Link className="HomeLink" to="/characters">
-          Characters
-        </Link>
-        <Link className="HomeLink" to="/locations">
-          {" "}
-          Locations
-        </Link>
-      </Wrapper>
-      <DescWrapper>
-        <p className="Desc">
-          The Rick and Morty Wiki is an unofficial collaborative database for
-          the hit [adult swim] series Rick and Morty, a science-fiction, black
-          comedy series about genius inventor, Rick Sanchez, and his naive
-          grandson, Morty Smith. The American animated television show is
-          created by Justin Roiland and Dan Harmon. It premiered on Cartoon
-          Network's [adult swim] block on December 2, 2013.
-        </p>
-      </DescWrapper>
-    </div>
-  );
+    return (
+        <>
+            <Background />
+            <FlexWrapper>
+                <Logo src={logo} />
+                <Wrapper>
+                    <ButtonContainer>
+                        <Link className="homelink" to="/characters">
+                            Characters
+                        </Link>
+                        <Link className="homelink" to="/locations">
+                            Locations
+                        </Link>
+                    </ButtonContainer>
+                    <Description>
+                        The Rick and Morty Wiki is an unofficial collaborative
+                        database for the hit [adult swim] series Rick and Morty,
+                        a science-fiction, black comedy series about genius
+                        inventor, Rick Sanchez, and his naive grandson, Morty
+                        Smith. The American animated television show is created
+                        by Justin Roiland and Dan Harmon. It premiered on
+                        Cartoon Network's [adult swim] block on December 2,
+                        2013.
+                    </Description>
+                </Wrapper>
+            </FlexWrapper>
+        </>
+    );
 };
 
 export default Home;
--- react-and-morty/src/components/Home.styles.js
+++ react-and-morty/src/components/Home.styles.js
@@ -1,80 +1,80 @@
 import styled from "styled-components";
+import background from "../images/background.jpg";
 
 export const Background = styled.img`
-  width: 100%;
-  height: 100vh;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
+    width: 100%;
+    height: 100vh;
+    background-image: url(${background});
+    background-position: center;
+    background-size: cover;
+`;
+export const FlexWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 `;
 
-export const Wrapper = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  display: flex;
-  justify-content: space-evenly;
-  font-family: "Bangers", cursive;
-  width: 50%;
-
-  .HomeLink {
-    width: 15rem;
-    height: 3rem;
+export const ButtonContainer = styled.div`
     display: flex;
-    justify-content: center;
-    align-items: center;
+    justify-content: space-evenly;
+    font-family: "Bangers", cursive;
+    margin: 1rem auto;
+    .homelink {
+        width: 15rem;
+        height: 3rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
 
-    text-decoration: none;
-    color: white;
-    background-color: rgba(82, 15, 7);
-    color: rgb(253, 174, 35);
-    border-radius: 2rem;
-    font-weight: bolder;
-    letter-spacing: 0.2rem;
-    border: 2px solid orange;
-    transition: 0.5s ease-in-out;
+        text-decoration: none;
+        color: white;
+        background-color: rgb(82, 15, 7);
+        color: rgb(253, 174, 35);
+        border-radius: 2rem;
+        font-weight: bolder;
+        letter-spacing: -2%;
+        border: 2px solid orange;
+        transition: 0.5s ease-in-out;
+
+        @media only screen and (max-width: 600px) {
+            margin-bottom: 1rem;
+        }
+
+        &:hover {
+            background-color: rgba(200, 25, 19);
+            color: black;
+        }
+    }
 
     @media only screen and (max-width: 600px) {
-      margin-bottom: 1rem;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
-
-    &:hover {
-      background-color: rgba(200, 25, 19);
-      color: black;
-    }
-  }
-
-  @media only screen and (max-width: 600px) {
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
 `;
 
 export const Logo = styled.img`
-  position: absolute;
-  top: 20%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  display: flex;
-  justify-content: center;
-  width: 50%;
-  height: auto;
+    width: 80%;
+    height: auto;
+    margin: 0 auto;
 `;
 
-export const DescWrapper = styled.div`
-  color: rgba(82, 15, 7);
-  position: absolute;
-  top: 70%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: rgba(255, 255, 255, 0.2);
-  backdrop-filter: blur(6px);
-  padding: 1.5rem;
-  border-radius: 1rem;
-  text-align: justify;
-  font-family: "Montserrat", sans-serif;
-  font-weight: bolder;
-  letter-spacing: 0.1rem;
+export const Wrapper = styled.div`
+    color: rgb(82, 15, 7);
+    display: flex;
+    flex-direction: column;
+`;
+export const Description = styled.p`
+    background-color: rgba(255, 255, 255, 0.2);
+    backdrop-filter: blur(6px);
+    max-height: 30%;
+    padding: 1.5rem;
+    border-radius: 1rem;
+    text-align: justify;
+    font-family: "Montserrat", sans-serif;
+    font-weight: bolder;
+    letter-spacing: -2%;
 `;
